e9747a397fcc6e438fe32047f6bc4bd331a7531d
SahajBiyani <88596592+SahajBiyani@users.noreply.github.com>
2021-08-13 13:19:50 -0500

    Update gaysite.py

--- gaysite.py
+++ gaysite.py
@@ -12,8 +12,8 @@
 from selenium import webdriver
 driver= webdriver.Chrome("chromedriver.exe")
 
-username = 'bhjbdh555f' #replace the text 'replace me i am useless" with the username u want, for exemple: DICKLOVER699
-TARGET = 'jxxibraxx7@baitify.com' #replace the text 'replace me i am useless" with the target email
+username = 'LaxmiSimp' #replace the text 'replace me i am useless" with the username u want, for exemple: DICKLOVER699
+TARGET = 'adityabhatia78@gmail.com' #replace the text 'replace me i am useless" with the target email
 driver.get('https://www.adam4adam.com/')
 password = 'azerhd5#'
 user_input = driver.find_element_by_xpath('/html/body/main/section[1]/div/div/div/form/div/div[1]/input')
